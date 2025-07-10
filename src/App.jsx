c9b2d8afb249bbc7821ade98386dc2a96ef9261e
nazmull139 <nazmul210121@diit.edu.bd>
2025-07-10 19:34:10 +0600

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,27 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-import LoginAdmin from './components/LoginAdmin'
-import Add from './pages/Add'
+ 
+ 
 import Cart from './pages/Cart'
-import Dashboard from './pages/Dashboard'
+ 
 import Home from './pages/Home'
-import OrderList from './pages/OrderList'
-import Update from './pages/Update'
+ 
+ 
 
 const App = () => {
   return (
-    <div className='text-red-500'>
+    <div >
      
       <Routes>
         <Route path='/' element={ <Home/> } />
         <Route path='/cart' element={ <Cart/> } />
-        <Route path='/login-admin' element={  <LoginAdmin/>  } />
+        {/** <Route path='/login-admin' element={  <LoginAdmin/>  } />
 
         <Route path='/dashboard' element={<Dashboard />}>
               <Route path='add-product' element={<Add />} />
               <Route path='update-product' element={<Update />} />
               <Route path='order-list' element={ <OrderList/>} />
-        </Route>
+        </Route>*/} 
         
  
       
